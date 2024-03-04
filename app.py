89227eb96e60e8eda7fde0cbc3715caece7671b4
Isadora Bagatini <150957928+IsahBag@users.noreply.github.com>
2024-03-04 17:33:09 +0000

    resolucao do desafio

--- app.py
+++ app.py
@@ -2,10 +2,54 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+import random
 
-from flask import Flask
-app = Flask(__name__)
+options = ["pedra", "papel", "tesoura"]
+score = 0 
+rounds_played = 0
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+while True:
+    random_choice = random.choice(options)  # inicializa o adversário
+    player_choice = input("Escolha pedra, papel ou tesoura: ")  # escolha do usuário
+
+    # escolheu pedra:
+    if player_choice == "pedra":
+        rounds_played += 1
+        if random_choice == "pedra":
+            print("Empate!")
+        elif random_choice == "papel":
+            print("Você perdeu")
+        elif random_choice == "tesoura":
+            print("Você ganhou!")
+            score += 1
+
+    # escolheu papel: 
+    elif player_choice == "papel":
+        rounds_played += 1
+        if random_choice == "papel":
+            print("Empate!")
+        elif random_choice == "tesoura":
+            print("Você perdeu")
+        elif random_choice == "pedra":
+            print("Você ganhou!")
+            score += 1
+    
+    # escolheu tesoura:
+    elif player_choice == "tesoura":
+        rounds_played += 1
+        if random_choice == "tesoura":
+            print("Empate!")
+        elif random_choice == "pedra":
+            print("Você perdeu")
+        elif random_choice == "papel":
+            print("Você ganhou!")
+            score += 1
+
+    # escolheu algo diferente de pedra, papel ou tesoura:
+    else:
+        play_again = input("Deseja jogar novamente? (s/n) ")
+        if play_again == "s":
+            continue
+        elif play_again == "n":
+            print(f"Você ganhou {score} vez(es), em {rounds_played} rodada(s) jogada(s)")    
+            break       
